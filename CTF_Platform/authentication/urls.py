f9be625e4f3ae16fdc7340dce420aa175a3f43e9
Ankur-Agrawal-ece20 <ankur.agrawal.ece20@itbhu.ac.in>
2021-10-12 19:55:40 +0530

    Added required functionalities!

--- CTF_Platform/authentication/urls.py
+++ CTF_Platform/authentication/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from authentication.views import *
 
 urlpatterns = [
-    path('',loginView, name="login"),
+    path('login',loginView, name="login"),
     path('logout',logoutView, name="logout"),
     path('register',registerView, name="register"),   
 ]
