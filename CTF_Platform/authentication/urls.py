0d8ee759cc83895a27260c724f425f004c83646c
RainaJain5 <rj35100@gmail.com>
2021-10-12 11:59:29 +0530

    html templates and views

--- CTF_Platform/authentication/urls.py
+++ CTF_Platform/authentication/urls.py
@@ -1,3 +1,9 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from authentication.views import *
 
-urlpatterns = []
+urlpatterns = [
+    path('',loginView, name="login"),
+    path('logout',logoutView, name="logout"),
+    path('register',registerView, name="register"),   
+]
