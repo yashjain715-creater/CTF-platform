586b11936e9016a03afd2f63903733966a9dd1e2
Ankur-Agrawal-ece20 <ankur.agrawal.ece20@itbhu.ac.in>
2021-10-12 09:19:29 +0530

    Added login, logout and register functionalities.

--- CTF_Platform/authentication/urls.py
+++ CTF_Platform/authentication/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
-
-urlpatterns = []
+from . views import loginView,logoutView,registerView
+urlpatterns = [
+    path('login',loginView, name="login"),
+    path('logout',logoutView, name="logout"),
+    path('register',registerView, name="register"),
+]
