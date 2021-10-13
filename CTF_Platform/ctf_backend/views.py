29a063c03b710c30e488d635b0dbba95723fe7a7
SanidhyaTaparia <tapariasanidhya@gmail.com>
2021-10-14 00:58:15 +0530

    Added Email Confirmation Pages

--- CTF_Platform/ctf_backend/views.py
+++ CTF_Platform/ctf_backend/views.py
@@ -5,6 +5,18 @@ from django.shortcuts import render
 def listQuesView(request):
     return render(request,'listQues.html')
 
+def emailConfirmationView(request):
+    return render(request,'emailConfirmation.html')
+
+def forgotPasswordView(request):
+    return render(request,'forgotPassword.html')
+
+def passwordResetView(request):
+    return render(request,'passwordReset.html')
+
+def successfulView(request):
+    return render(request,'successful.html')
+
 def submitAnswerView(request):
     pass
 
