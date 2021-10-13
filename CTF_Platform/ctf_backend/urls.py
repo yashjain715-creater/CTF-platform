3931abcd00a359605ffca139ad298ce61549297d
SanidhyaTaparia <tapariasanidhya@gmail.com>
2021-10-14 00:13:07 +0530

    Revert "Added Email Confirmation Page"
    This reverts commit 4dbbb1c3f607ee336a95b88b28133cb45b73a2e2.

--- CTF_Platform/ctf_backend/urls.py
+++ CTF_Platform/ctf_backend/urls.py
@@ -3,9 +3,5 @@ from ctf_backend.views import *
 
 urlpatterns = [
     path('',listQuesView, name="listQues"),
-    path('submit',submitAnswerView, name="submitAns"),
-    path('emailConfirmation',emailConfirmationView,name="emailConfirmation"),
-    path('forgotPassword',forgotPasswordView,name="forgotPassword"),  
-    path('passwordReset',passwordResetView,name="passwordReset"),
-    path('successful',successfulView,name="successful"),  
+    path('submit',submitAnswerView, name="submitAns"),  
 ]
